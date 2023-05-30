a4f85976955c6251563f8f86ac097be393c49f1e
Acticode <132838068+Acticode@users.noreply.github.com>
2023-05-30 13:40:48 +0900

    10_4

--- main.c
+++ main.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("hello world!");
-    printf("2nd line");
+    char input_data[200];
+    int i = 0, start, end;
+
+    printf(" : "); // ######
+    scanf("%s", &input_data);
+
+    while (input_data[i] != '.') {
+        i++;
+    }
+    start = i + 1;
+    end = i;
+
+    while (i >= 0) {
+        if (input_data[i] == ' ' || i == 0) {
+            if (i == 0) {
+                start = 0;
+                for (int j = start; j >= end; j++) {
+                    printf("%s", input_data[j]);
+                }
+                printf(".");
+            }
+            else {
+                start = i + 1;
+
+                for (int j = start; j >= end; j++) {
+                    printf("%s", input_data[j]);
+                }
+                printf(" ");
+                end = i - 1;
+            }
+        }
+        i--;
+    }
+
 
     return 0;
 }
